5589827d807ba58081f94c79b4fe9a9902282c2f
kotaro <ktrst0622@gmail.com>
2024-12-27 12:19:29 +0900

    initial commit

---
+++ database/init/ddl.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS 'movie-review';
+CREATE DATABASE 'movie-review';
+
+USE 'movie-review';
---
+++ src/main/java/com/example/movie/review/be/MovieReviewBeApplication.java
@@ -0,0 +1,12 @@
+package com.example.movie.review.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MovieReviewBeApplication.class, args);
+  }
+}
