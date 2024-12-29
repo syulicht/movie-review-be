a46eca98dbfdec410e9a704d14bc8c055ed0adc0
kotaro <ktrst0622@gmail.com>
2024-12-29 21:12:41 +0900

    fix

--- src/main/java/com/example/movie/review/be/security/AuthorizeFilter.java
+++ src/main/java/com/example/movie/review/be/security/AuthorizeFilter.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,9 +18,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
-@RequiredArgsConstructor
 public class AuthorizeFilter extends OncePerRequestFilter {
-  private final HmacConfiguration hmacConfiguration;
+  @Autowired private HmacConfiguration hmacConfiguration;
 
   @Override
   protected void doFilterInternal(
