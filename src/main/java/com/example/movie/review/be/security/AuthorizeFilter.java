7b0a2644b74e180078dce66444ef9ee8c1087454
kotaro <ktrst0622@gmail.com>
2024-12-29 21:09:35 +0900

    fix

--- src/main/java/com/example/movie/review/be/security/AuthorizeFilter.java
+++ src/main/java/com/example/movie/review/be/security/AuthorizeFilter.java
@@ -3,12 +3,14 @@ package com.example.movie.review.be.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.movie.review.be.configuration.HmacConfiguration;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,7 +18,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
+@RequiredArgsConstructor
 public class AuthorizeFilter extends OncePerRequestFilter {
+  private final HmacConfiguration hmacConfiguration;
+
   @Override
   protected void doFilterInternal(
       HttpServletRequest request,
@@ -29,7 +34,9 @@ public class AuthorizeFilter extends OncePerRequestFilter {
       return;
     }
     DecodedJWT decodedJwt =
-        JWT.require(Algorithm.HMAC256("__secret__")).build().verify(authToken.substring(7));
+        JWT.require(Algorithm.HMAC256(hmacConfiguration.getSecret()))
+            .build()
+            .verify(authToken.substring(7));
     String userId = decodedJwt.getClaim("userid").toString();
     SecurityContextHolder.getContext()
         .setAuthentication(new UsernamePasswordAuthenticationToken(userId, null, List.of()));
